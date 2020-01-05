fc17180031df95021ce5535c4bb04b5423771076
Gichimu <gichimueric12@gmail.com>
2020-01-05 12:21:11 +0300

    configure the navbar

--- pics/views.py
+++ pics/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def welcome(request):
-    return HttpResponse('Welcome to the site')
+    return render(request, 'welcome.html')
 
 
